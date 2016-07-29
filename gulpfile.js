f1bd52ae570b5dfec546a5084703977925a663ba
Saulo Matte Madalozzo <saulo.zz@gmail.com>
2016-07-29 06:53:23 -0300

    finalized gulp and optimizations in station page
    - new assets folder before gulp
    - gulpfile optimin
    - added headerType

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,68 @@
-/* File: gulpfile.js */
-
 // grab our gulp packages
-var gulp = require('gulp'),
-autoprefixer = require('gulp-autoprefixer'),
-cssnano = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-	ngAnnotate = require('gulp-ng-annotate'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    del = require('del'),
-	htmlmin = require('gulp-htmlmin');
+var gulp = require('gulp');
+var	autoprefixer = require('gulp-autoprefixer');
+var	cssnano = require('gulp-cssnano');
+// var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+// var cache = require('gulp-cache');
+// var imagemin = require('gulp-imagemin');
+var del = require('del');
+var htmlmin = require('gulp-htmlmin');
 
-
-gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts', 'html', 'watch');
+gulp.task('default', ['clean'], function () {
+	gulp.start('styles', 'scripts', 'html', 'watch');
 });
 
-gulp.task('clean', function() {
-    return del(['public/assets/css', 'public/assets/js', 'public/assets/img']);
+gulp.task('clean', function () {
+	return del(['public/assets/css', 'public/assets/js', 'public/assets/img']);
 });
 
-gulp.task('html', function() {
-  return gulp.src('assets/html/*.html')
-    .pipe(htmlmin({
-		collapseWhitespace: true,
-		removeComments: true,
-		sortAttributes: true,
-		sortClassName: true
-		
-				}))
-    .pipe(gulp.dest('public'))
+gulp.task('html', function () {
+	return gulp.src('assets/html/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			removeComments: true,
+			sortAttributes: true,
+			sortClassName: true
+		}))
+		.pipe(gulp.dest('public'));
 	// .pipe(notify({ message: 'Html task complete' }));
 });
 
 // Styles
-gulp.task('styles', function() {
-  return gulp.src('assets/styles/*.css')
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('public/assets/css/'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(cssnano())
-    .pipe(gulp.dest('public/assets/css/'))
-    .pipe(notify({ message: 'Styles task complete' }));
+gulp.task('styles', function () {
+	return gulp.src('assets/styles/*.css')
+		.pipe(autoprefixer('last 2 version'))
+		.pipe(gulp.dest('public/assets/css/'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cssnano())
+		.pipe(gulp.dest('public/assets/css/'))
+		.pipe(notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('scripts', function () {
-  return gulp.src('assets/scripts/*.js')
-    //.pipe(jshint('.jshintrc'))
-    //.pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('public/assets/js'))
-    .pipe(rename({suffix: '.min'}))
-	.pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('public/assets/js'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+	return gulp.src('assets/scripts/*.js')
+		// .pipe(jshint('.jshintrc'))
+		// .pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('public/assets/js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(ngAnnotate())
+		.pipe(uglify())
+		.pipe(gulp.dest('public/assets/js'))
+		.pipe(notify({ message: 'Scripts task complete' }));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('assets/styles/*.css', ['styles']);
-  gulp.watch('assets/scripts/*.js', ['scripts']);
-  gulp.watch('assets/html/*.html', ['html']);
-  //gulp.watch('src/images/**/*', ['images']);
-
+gulp.task('watch', function () {
+	gulp.watch('assets/styles/*.css', ['styles']);
+	gulp.watch('assets/scripts/*.js', ['scripts']);
+	gulp.watch('assets/html/*.html', ['html']);
+	// gulp.watch('src/images/**/*', ['images']);
 });
-
 
 // gulp.task('images', function() {
 //   return gulp.src('src/images/**/*')
